a01719f8e1db632668e4b3c2c87c20e81a522915
mors1996 <moreno.gregori96@gmail.com>
2023-02-03 15:10:27 +0100

    notes sticky

--- components/MemoCard.js
+++ components/MemoCard.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, Icon, IconButton, Surface } from "@react-native-material/core";
 import { IconComponentProvider } from "@react-native-material/core";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Animated, PanResponder } from 'react-native';
+
+
 
 export default class MemoCard extends React.Component {
+    pan = new Animated.ValueXY();
+
+    panResponder =
+        PanResponder.create({
+            onMoveShouldSetPanResponder: () => true,
+            onPanResponderMove: Animated.event([null, { dx: this.pan.x, dy: this.pan.y }]),
+            onPanResponderRelease: () => {
+                this.pan.extractOffset();
+            },
+        });
+
     constructor(props) {
         super(props);
         // Don't call this.setState() here!
         this.state = { clicked: false };
+
     }
+
+
     render = () => {
-        return (<Surface elevation={1}
-            category="medium"
-            style={styles.item}><Text>{this.props.title}</Text><IconButton icon={(<IconComponentProvider IconComponent={MaterialCommunityIcons}>
-                <Icon name="pen" size={24}></Icon></IconComponentProvider>)} color="white" title={"See note"}></IconButton>
-        </Surface>);
+        return (
+            <View >
+                <Animated.View
+                    style={{
+                        transform: [{ translateX: this.pan.x }, { translateY: this.pan.y }],
+                    }}
+                    {...this.panResponder.panHandlers}>
+                    <Surface elevation={1}
+                        category="medium"
+                        style={styles.item}>
+                        <IconButton style={styles.closeButton} onPress={this.props.onPressDelete} icon={(<IconComponentProvider IconComponent={MaterialCommunityIcons}><Icon name="close" size={24}></Icon></IconComponentProvider>)} />
+                        <Text>{this.props.title}</Text><IconButton onPress={this.props.onPress} icon={(<IconComponentProvider IconComponent={MaterialCommunityIcons}>
+                            <Icon name={this.props.new !== 'true' ? 'pen' : 'plus'} size={24}></Icon></IconComponentProvider>)} color="white" title={"See note"}></IconButton>
+                    </Surface>
+                </Animated.View>
+            </View>);
     }
+
+
 }
 
 
@@ -32,6 +62,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'yellow',
         padding: 20,
         marginVertical: 8,
+        maxWidth:'200px'
     },
     header: {
         fontSize: 32,
@@ -40,6 +71,12 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
     },
+    closeButton: {
+        alignSelf: 'flex-end',
+        alignContent: 'flex-end',
+
+
+    }
 });
 
 
--- components/NoteView.js
+++ components/NoteView.js
@@ -1,43 +1,123 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, VirtualizedList } from 'react-native';
 import MemoCard from "./MemoCard";
+import {
+    Provider,
+    Dialog,
+    DialogHeader,
+    DialogContent,
+    DialogActions,
+    Button,
+    TextInput,
+    IconButton,
+    Icon,
+    PortalProvider,
+    useWindowSize
+} from "@react-native-material/core";
 
-const Item = ({title}) => (
+
+const Item = ({ title }) => (
     <MemoCard title={title}>
     </MemoCard>
 );
 
-const getItem = (_data, index) => ({
-    id: Math.random().toString(12).substring(0),
-    title: `Item ${index + 1}`,
-});
 
-const getItemCount = (_data) => 50;
+
 
 export default class NoteView extends React.Component {
 
     constructor(props) {
         super(props);
         // Don't call this.setState() here!
-        this.state = { notes: [] };
+        this.state = { notes: [{ "id": 1, "title": "Nota 1", "text": "aaaaaaaaaaaaaaaaaasaasda" }, { "id": 2, "title": "Nota 2", "text": "aaaaaaaaaaaaaaaaaasaasda" }, { "id": 3, "title": "Nota 3" }], clicked: false, selectedNote: {}, newNote: false };
     }
+
+    getItem = (_data, index) => (this.state.notes[index]
+    );
+
+    getItemCount = (_data) => this.state.notes.length;
+
     render = () => {
         return (
             <View style={styles.container}>
                 <Text style={styles.boldSubTitle}>Your Notes</Text>
-                <MemoCard title="Add new note" />
-                <SafeAreaView style={styles.container}>
+                <MemoCard onPress={() => this.setVisible(true, {}, true)} new="true" title="Add new note" />
+                <SafeAreaView style={styles.safeArea}>
                     <VirtualizedList
-                        initialNumToRender={3}
-                        renderItem={({ item }) => <MemoCard title={item.title} />}
+                        initialNumToRender={this.state.notes.length}
+                        renderItem={({ item }) => <MemoCard onPress={() => this.setVisible(true, item)} onPressDelete={() => this.removeItem(item)} new="false" title={item.title} />}
                         keyExtractor={item => item.id}
-                        getItemCount={getItemCount}
-                        getItem={getItem}
+                        getItemCount={this.getItemCount}
+                        getItem={this.getItem}
+                        style={({ width: '100%' })}
                     />
+                    <Provider>
+                        <Dialog visible={this.state.clicked} onDismiss={() => this.setVisible(false)}>
+                            <DialogHeader title={(<TextInput
+                                style={styles.dialogText}
+                                defaultValue={this.state.selectedNote.title}
+                                onChangeText={(value) => { this.state.selectedNote.title = value }}
+
+                            />)} />
+                            <DialogContent>
+
+                                <TextInput
+                                    style={styles.dialogText}
+                                    defaultValue={this.state.selectedNote.text}
+                                    onChangeText={(value) => { this.state.selectedNote.text = value }}
+
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button
+                                    title="Cancel"
+                                    compact
+                                    variant="text"
+                                    onPress={() => this.setVisible(false)}
+                                />
+                                <Button
+                                    title="Confirm"
+                                    compact
+                                    variant="text"
+                                    onPress={() => {
+                                        if (!this.state.newNote) this.setNewItem(this.state.selectedNote);
+                                        else this.addItem(this.state.selectedNote)
+                                        this.setVisible(false)
+                                    }}
+                                />
+                            </DialogActions>
+                        </Dialog></Provider>
                 </SafeAreaView>
             </View>
         );
     }
+    setVisible(x, selectedNote = this.state.selectedNote, newNote = false) { this.setState({ clicked: x, selectedNote: selectedNote, newNote: newNote }) }
+    setNewItem(
+        item
+    ) {
+        var index = this.state.notes.findIndex(x => x.id == item.id)
+        this.state.notes[index] = item;
+    }
+    addItem(item) {
+        var index = Math.max(...this.state.notes.map(i => i.id))
+        item.id = index + 1;
+        var list = this.state.notes;
+        list.push(item)
+        this.setState({ notes: list })
+    }
+
+    removeItem(
+        item
+    ) {
+        var index = this.state.notes.findIndex(x => x.id == item.id)
+        console.log(index)
+        var list = this.state.notes;
+        list.splice(index, 1); // 2nd parameter means remove one item only
+        this.setState({ notes: list })
+
+    }
+
+
 }
 
 
@@ -48,6 +128,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginHorizontal: 16,
+    },
+
+    safeArea:
+    {
+        width: '100%'
     },
     item: {
         backgroundColor: '#f9c2ff',
@@ -62,9 +147,14 @@ const styles = StyleSheet.create({
         fontSize: 24,
     },
     boldSubTitle: {
-        padding:20,
-        fontSize:18, 
+        padding: 20,
+        fontSize: 18,
         fontWeight: 'bold'
+    },
+    dialogText:
+    {
+        color: 'white',
+
     }
 });
 
